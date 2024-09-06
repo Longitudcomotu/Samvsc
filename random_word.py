8ba0f8843b3f522bff22b4a2ade2b58a0d3d5d9e
Longitudcomotu <teambananin4@hotmail.com>
2024-09-06 09:35:40 -0600

    this is random word

---
+++ random_word.py
@@ -0,0 +1,18 @@
+
+print("Guess the word")
+import random
+
+print("List of words to choose from:")
+
+word_list = ["python","java","javascript","ruby","html","css"]
+print(word_list)
+# Randomly select a word from the list
+selected_word = random.choice(word_list)
+# Initialize the user's guess
+user_guess = input("Enter your guess: ")
+while user_guess != selected_word:
+    user_guess = input("try again: ")
+    if user_guess == selected_word:
+        print("you won bro")
+        break
+    
