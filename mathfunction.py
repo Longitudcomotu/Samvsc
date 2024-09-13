110ddff925e67ecd2bbe4f87f16a285d83fce6e4
Longitudcomotu <teambananin4@hotmail.com>
2024-09-13 10:57:16 -0600

    mathfunctions

--- mathfunction.py
+++ mathfunction.py
@@ -5,3 +5,21 @@ def factorial(n):
         print(result)
         
 factorial(6)
+factorial(11)
+factorial(15)
+factorial(22)
+
+
+def reminder(a,b):
+    print("The result is")
+    print(a -((a//b)*b))
+ 
+reminder(3,2)
+reminder(21,4)
+reminder(131,19)
+reminder(81,9)
+
+3 % 2
+21 % 4
+131 % 19
+81 % 9
